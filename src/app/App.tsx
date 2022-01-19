2c805d524b83b024de20e3a7306b110f91aa2f77
Mayariff <79027972026@yandex.ru>
2022-01-19 18:35:09 +0500

    add spin and error processing

---
+++ src/app/App.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import './App.css'
+import { TodolistsList } from '../features/TodolistsList/TodolistsList'
+
+
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import { Menu } from '@mui/icons-material';
+import {LinearProgress} from "@mui/material";
+import { RequestStatusType} from "./app-reducer";
+import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "./store";
+
+
+
+
+function App() {
+   const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
+    //const status = useAppSelector<RequestStatusType>(state=> state.app.status)
+
+
+    return (
+        <div className="App">
+            <ErrorSnackbar />
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+            { status === 'loading' && <LinearProgress color="secondary"/>}
+            <Container fixed>
+                <TodolistsList/>
+            </Container>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/EditableSpan/EditableSpan.tsx
@@ -0,0 +1,34 @@
+import React, { ChangeEvent, useState } from 'react';
+import TextField from '@mui/material/TextField';
+import {RequestStatusType} from "../../app/app-reducer";
+
+
+
+type EditableSpanPropsType = {
+    value: string
+    onChange: (newValue: string) => void
+    entityStatus: RequestStatusType
+}
+
+export const EditableSpan = React.memo(function (props: EditableSpanPropsType) {
+    console.log('EditableSpan called');
+    let [editMode, setEditMode] = useState(false);
+    let [title, setTitle] = useState(props.value);
+
+    const activateEditMode = () => {
+            setEditMode(true);
+            setTitle(props.value);
+
+    }
+    const activateViewMode = () => {
+            setEditMode(false);
+            props.onChange(title);
+    }
+    const changeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+
+    return   editMode ? <TextField  value={title} onChange={changeTitle} autoFocus onBlur={activateViewMode}  />
+        :<span onDoubleClick={activateEditMode}> {props.value}</span>
+
+});
